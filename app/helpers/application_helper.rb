a2c4f1108e5265254f3486730080948628d988fe
eclectic-coding <eclectic-coding@users.noreply.github.com>
2026-02-13 12:10:29 -0500

    Work on article preview

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,4 +22,34 @@ module ApplicationHelper
       .titleize
       .presence || "Application"
   end
+
+  # Generates a preview of article content, removing code blocks and truncating to specified length
+  def article_preview(content, length: 200)
+    return "" if content.blank?
+
+    # Remove code blocks (fenced with ``` or indented)
+    # Remove fenced code blocks (```...```)
+    text_without_code = content.gsub(/```[\s\S]*?```/, '')
+
+    # Remove indented code blocks (4 spaces or tab at start of line)
+    text_without_code = text_without_code.gsub(/^([ ]{4}|\t).*$/, '')
+
+    # Remove inline code (`...`)
+    text_without_code = text_without_code.gsub(/`[^`]*`/, '')
+
+    # Remove markdown headers, links, bold, italic formatting
+    text_without_code = text_without_code.gsub(/#+\s*/, '')  # Headers
+    text_without_code = text_without_code.gsub(/\[([^\]]+)\]\([^)]+\)/, '\1')  # Links
+    text_without_code = text_without_code.gsub(/[*_]{1,2}([^*_]+)[*_]{1,2}/, '\1')  # Bold/Italic
+
+    # Clean up extra whitespace
+    text_without_code = text_without_code.gsub(/\n+/, ' ').strip.squeeze(' ')
+
+    # Truncate to desired length
+    if text_without_code.length > length
+      text_without_code.truncate(length, separator: ' ', omission: '...')
+    else
+      text_without_code
+    end
+  end
 end
