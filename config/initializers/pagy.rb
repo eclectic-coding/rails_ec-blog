44f61623334ef336b224a529e4a061b345807e1a
eclectic-coding <eclectic-coding@users.noreply.github.com>
2026-06-04 13:39:04 -0400

    Upgrade deprecated Pagy options

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -4,11 +4,11 @@
 # Customize only what you really need and notice that the core Pagy works also without any of the following lines.
 
 # Configure pagy default options
-Pagy.options[:limit] = 5    # default limit for pagination
+Pagy::OPTIONS[:limit] = 5    # default limit for pagination
 
 # Uncomment and customize extras as needed
 # See https://ddnexus.github.io/pagy/docs/extras
 # require 'pagy/extras/overflow'
-# Pagy.options[:overflow] = :last_page
+# Pagy.OPTIONS[:overflow] = :last_page
 
 
