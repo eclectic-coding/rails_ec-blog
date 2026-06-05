9a58bd55524411f13fc4285eeaafa7c4a2318b6b
eclectic-coding <eclectic-coding@users.noreply.github.com>
2026-06-05 15:58:15 -0400

    Remove name of person gem

--- config/gems/app.rb
+++ config/gems/app.rb
@@ -1,5 +1,5 @@
 gem "inline_svg"
-gem "name_of_person"
+# gem "name_of_person"
 # gem "strong_migrations" # Uncomment if you want to use strong_migrations
 gem "bootstrap", "~> 5.3.3"
 gem "dartsass-rails"
